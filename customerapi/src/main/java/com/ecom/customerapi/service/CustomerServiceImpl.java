6eafe1811b38f6b6b814033057e1154c5b7391aa
varshith reddy <varshithreddy1009@gmail.com>
2026-05-12 15:18:15 +0530

    added logs in customerserviceimpl

--- customerapi/src/main/java/com/ecom/customerapi/service/CustomerServiceImpl.java
+++ customerapi/src/main/java/com/ecom/customerapi/service/CustomerServiceImpl.java
@@ -4,12 +4,14 @@ import com.ecom.customerapi.dto.CustomerDto;
 import com.ecom.customerapi.entity.CustomerEntity;
 import com.ecom.customerapi.exception.ResourceNotFoundException;
 import com.ecom.customerapi.repository.CustomerRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService
 {
@@ -28,8 +30,12 @@ public class CustomerServiceImpl implements CustomerService
             2.if not there create a new account
          */
 
+        log.info("control entered addNewCustomerserv = "+customerdto);
+
         CustomerEntity customerentity = modelmapper.map(customerdto , CustomerEntity.class);
         CustomerEntity savedcustomer = customerrepo.save(customerentity);
+
+        log.info("user with this name is saved in customer-service = "+customerdto.getName());
         return modelmapper.map(savedcustomer , CustomerDto.class);
     }
 
